0c076293a928de6f949d9477ce83eebe16aaf0e7
dynamic2code <dynamicdesingns@gmail.com>
2023-11-02 16:29:25 +0300

    card and scaffold

--- app/src/main/java/com/example/flowers/MainActivity.kt
+++ app/src/main/java/com/example/flowers/MainActivity.kt
@@ -3,44 +3,116 @@ package com.example.flowers
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.flowers.ui.theme.FlowersTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FlowersTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Item()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LandingPage(){
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        textAlign = TextAlign.Center,
+                        text = "Flowers")
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(Icons.Filled.Home, contentDescription = "Localized description")
+                }
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(Icons.Filled.ShoppingCart, contentDescription = "Localized description")
+                }
+            }
+        },
+    ) {paddingValues ->
+        LazyColumn {
+
+        }
+    }
+
+}
+@Composable
+fun Item(){
+    Card(
+        modifier = Modifier.size(width = 450.dp, height = 360.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+
+        ) {
+            ElevatedCard (
+                modifier = Modifier.size(width = 350.dp, height = 350.dp)
+
+            ){
+
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "flower name")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FlowersTheme {
-        Greeting("world")
-    }
+fun pageOne(){
+    LandingPage()
+    Item()
 }
